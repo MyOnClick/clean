ce44eb49ff5f77a37749461dfbf4bfb2179fa298
MyOnClikc <inchood@yandex.ru>
2019-05-25 22:39:11 +0300

    Main-1(no-adaptive)

---
+++ js/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+// hamburger
+	$("#navToggle").click(function () {
+		$(this).toggleClass("active");
+		$(".overlay").toggleClass("open");
+		// this line ▼ prevents content scroll-behind
+		$("body").toggleClass("locked");
+	});
+
+	// Carusel
+	$('.main-slider').slick({
+		// autoplay:true,
+		arrows:false,
+		dots: true,
+		infinite: true,
+		// autoplaySpeed:2000,
+		// speed: 1000,
+		fade: true,
+		cssEase: 'linear'
+	});
+});
