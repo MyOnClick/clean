05d0b18a859f3318ae1d43043ac220388d9571b5
MyOnClikc <inchood@yandex.ru>
2019-06-02 22:25:55 +0300

    OK

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+	
 // hamburger
 	$("#navToggle").click(function () {
 		$(this).toggleClass("active");
@@ -7,7 +7,83 @@ $(document).ready(function () {
 		// this line ▼ prevents content scroll-behind
 		$("body").toggleClass("locked");
 	});
+	//  toTop
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		});
+		$('#toTop').click(function () {
+			$('body,html').animate({ scrollTop: 0 }, 800);
+		});
+	});
+// Gallery
+	(function ($) {
+		var $li = $('.img-list').find('> li'),
+			$links = $li.find('> a'),
+			$lightbox = $('.lightbox'),
+			$next = $('.next'),
+			$prev = $('.prev'),
+			$overlay = $('.overlay'),
+			liIndex,
+			targetImg;
+		//preload images
+		var imgSources = [
+			'img/cleaningProduct/1.jpg',
+			'img/cleaningProduct/1.jpg',
+			'img/cleaningProduct/1.jpg',
+			'img/cleaningProduct/1.jpg',
+			'img/cleaningProduct/1.jpg',
+			'img/cleaningProduct/1.jpg',
+			'img/cleaningProduct/1.jpg'
+		];
+		var imgs = [];
+		for (var i = 0; i < imgSources.length; i++) {
+			imgs[i] = new Image();
+			imgs[i].src = imgSources[i];
+		}
+		function replaceImg(src) {
+			$lightbox.find('img').attr('src', src);
+		}
+		function getHref(index) {
+			return $li.eq(index).find('>a').attr('href');
+		}
+		function closeLigtbox() {
+			$lightbox.fadeOut();
+		}
+		$overlay.click(closeLigtbox);
+		$links.click(function (e) {
+			e.preventDefault();
+			targetImg = $(this).attr('href');
+			liIndex = $(this).parent().index();
+			replaceImg(targetImg);
+			$lightbox.fadeIn();
+		});
+		$next.click(function () {
+			if ((liIndex + 1) < $li.length) {
+				targetImg = getHref(liIndex + 1);
+				liIndex++;
+			} else {
+				targetImg = getHref(0);
+				liIndex = 0;
+			}
+			replaceImg(targetImg);
+		});
 
+		$prev.click(function () {
+			if ((liIndex) > 0) {
+				targetImg = getHref(liIndex - 1);
+				liIndex--;
+			} else {
+				targetImg = getHref($li.length - 1);
+				liIndex = $li.length - 1;
+			}
+			replaceImg(targetImg);
+		});
+	})(jQuery);
 	// Carusel
 	$('.main-slider').slick({
 		autoplay:true,
